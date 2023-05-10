08ad278fc41f4753ca5728d5d9430380f7a0c257
Your Name <you@example.com>
2023-05-10 15:03:20 -0300

    ...

---
+++ main.js
@@ -0,0 +1,6 @@
+const domText = document.querySelector("p.text")
+const text = `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur, at repellendus totam magni quaerat, quod, sit ullam animi reprehenderit omnis fuga unde. Voluptas exercitationem vero cumque id iure minima esse.`
+
+text.split("").forEach(letra => {
+  // ...
+})
