8e497e3ea9e6767dd3c1834ee0b98671027dd933
Your Name <you@example.com>
2023-05-11 16:41:32 -0300

    ...

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
+
 const domText = document.querySelector("p.text")
-const text = `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur, at repellendus totam magni quaerat, quod, sit ullam animi reprehenderit omnis fuga unde. Voluptas exercitationem vero cumque id iure minima esse.`
+let text = `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur, at repellendus totam magni quaerat, quod, sit ullam animi reprehenderit omnis fuga unde. Voluptas exercitationem vero cumque id iure minima esse.`
+let palavra = "<span class='word'>"
+text += " " // IMPORTANTE, LEMBRE-SE PQ ISSO EXISTE! #PAZ
 
 text.split("").forEach(letra => {
-  // ...
+  if (letra != " ") {
+    palavra += "<span class='letter'>" + letra + "</span>"
+    return
+  } 
+  palavra += "</span><span class='space'> </span>"
+  domText.innerHTML += palavra
+  palavra = "<span class='word'>"
 })
