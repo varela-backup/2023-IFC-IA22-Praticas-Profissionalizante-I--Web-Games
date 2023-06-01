812191a6b000f43c8bf47ba2b76fe9ba9128591a
Your Name <you@example.com>
2023-06-01 15:48:28 -0300

    ...

--- main.js
+++ main.js
@@ -1,15 +1,130 @@
+let endGame = false
+let gameStarted = false
 
-const domText = document.querySelector("p.text")
-let text = `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur, at repellendus totam magni quaerat, quod, sit ullam animi reprehenderit omnis fuga unde. Voluptas exercitationem vero cumque id iure minima esse.`
-let palavra = "<span class='word'>"
-text += " " // IMPORTANTE, LEMBRE-SE PQ ISSO EXISTE! #PAZ
-
-text.split("").forEach(letra => {
-  if (letra != " ") {
-    palavra += "<span class='letter'>" + letra + "</span>"
-    return
-  } 
-  palavra += "</span><span class='space'> </span>"
-  domText.innerHTML += palavra
-  palavra = "<span class='word'>"
+const settings = Object.freeze({
+  wordsCount: 60
 })
+
+async function acquireWords() {
+  let request = await fetch("input/lorem.txt")
+  let text = await request.text()
+  return text.trim().split(/\s+/img)
+}
+
+function getRandomWord(words) {
+  return words[Math.floor(Math.random() * words.length)].trim()
+}
+
+function getRandomParagraph(words) {
+  const randomWordsArray = []
+  let counter = settings.wordsCount
+  while (counter--)
+    randomWordsArray.push(getRandomWord(words).trim())
+  return randomWordsArray
+}
+
+function createDomWord(word) {
+  const letters = word.split("")
+  const div = document.createElement("div")
+  letters.forEach(letter => {
+    const span = document.createElement("span")
+    span.innerText = letter
+    div.append(span)
+  })
+  return div
+}
+
+function createSpace() {
+  const span = document.createElement("span")
+  const spanSpace = document.createElement("span")
+  spanSpace.innerHTML = "&nbsp;"
+  span.append(spanSpace)
+  return span
+}
+
+function createDomParagraph(paragraph) {
+  const p = document.createElement("p")
+  paragraph.map((word, x, y) => {
+    p.append(createDomWord(word))
+    if (x + 1 != y.length)
+      p.append(createSpace())
+  })
+  return p
+}
+
+function checkLetter(letter, domLetter) {
+  window.dispatchEvent(new CustomEvent("letter"))
+  if (letter.trim() == domLetter.innerText.trim()) {
+    domLetter.classList.add("correct")
+    window.dispatchEvent(new CustomEvent("correctletter"))
+    return
+  }
+  domLetter.classList.add("wrong")
+  window.dispatchEvent(new CustomEvent("wrongletter"))
+}
+
+function checkWord(word, domWord) {
+  window.dispatchEvent(new CustomEvent("word"))
+  if (word.trim() == domWord.innerText.trim()) {
+    domWord.classList.add("correct")
+    window.dispatchEvent(new CustomEvent("correctword"))
+    return
+  }
+  domWord.classList.add("wrong")
+  window.dispatchEvent(new CustomEvent("wrongword"))
+}
+
+function gameRules(paragraph) {
+  let currentWord = paragraph.querySelector(":first-child")
+  let currentLetter = currentWord.querySelector(":first-child")
+  let typedWord = ""
+  currentWord.classList.add("current")
+  currentLetter.classList.add("current")
+  window.addEventListener("keydown", ev => {
+    if (!gameStarted) {
+      gameStarted = true
+      window.dispatchEvent(new CustomEvent("gamestarted"))
+    }
+
+    if (endGame)
+      return
+
+    // ignore all special keys, except space
+    if (!ev.code.includes("Key") && ev.code != "Space")
+      return
+
+    typedWord += ev.key
+    checkLetter(ev.key, currentLetter)
+    currentLetter.classList.remove("current")
+    currentLetter = currentLetter.nextElementSibling
+
+    if (!currentLetter) {
+      checkWord(typedWord, currentWord)
+      typedWord = ""
+      currentWord.classList.remove("current")
+      currentWord = currentWord.nextElementSibling
+
+      if (!currentWord) {
+        endGame = true
+        window.dispatchEvent(new CustomEvent("endgame"))
+        return
+      }
+
+      currentWord.classList.add("current")
+      currentLetter = currentWord.querySelector(":first-child")
+    }
+
+    currentLetter.classList.add("current")
+  })
+}
+
+void async function () {
+  const words = await acquireWords()
+  const randomParagraph = getRandomParagraph(words)
+  const domParagraph = createDomParagraph(randomParagraph)
+  const domTextPlace = document.querySelector("main div.text")
+  if (!domTextPlace)
+    throw new Error("[DOM HTML MISSING]: Element `main div.text` not found in index.html")
+  domTextPlace.append(domParagraph)
+  gameRules(domParagraph)
+}();
---
+++ plugins/daniel-de-andrade-varela.js
@@ -0,0 +1,19 @@
+// window.addEventListener("gamestarted", ev => console.log("gamestarted"))
+// window.addEventListener("letter", ev => console.log("letter"))
+// window.addEventListener("correctletter", ev => console.log("correctletter"))
+// window.addEventListener("wrongletter", ev => console.log("wrongletter"))
+// window.addEventListener("word", ev => console.log("word"))
+// window.addEventListener("correctword", ev => console.log("correctword"))
+// window.addEventListener("wrongword", ev => console.log("wrongword"))
+// window.addEventListener("endgame", ev => console.log("endgame"))
+
+const score = document.querySelector("main div.score")
+const corretTypedLetters = document.createElement("span")
+let couterCorrectTypedWords = 0
+
+score.append(corretTypedLetters)
+
+window.addEventListener("correctletter", ev => {
+  couterCorrectTypedWords++
+  corretTypedLetters.innerText = couterCorrectTypedWords
+})
